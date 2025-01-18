d7d845dda02ced964104bbd6e5ea9dcdbc0de021
Maria <mariya-ivanova@list.ru>
2025-01-18 22:47:16 +0300

    the solution of third task was added and checked

---
+++ task3.py
@@ -0,0 +1,9 @@
+from collections import Counter
+
+def can_be_poly(row: str) -> bool:
+    char_counts = Counter(row)
+    odd_count = len(list(filter(lambda x: x % 2, char_counts.values())))
+    return odd_count < 2
+
+
+print(can_be_poly(input('Введите ваше слово: ')))
